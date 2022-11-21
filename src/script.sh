45dc1608120cacfcbdc365d60cf347dc4dae8e5e
cyberslot <cyberslot@gmail.com>
2022-11-21 15:01:06 +0000

    Update script.sh artifact

--- src/script.sh
+++ src/script.sh
@@ -1,5 +1,23 @@
-if [ "$(docker ps -q)" != "" ]; then
-    docker stop $(docker ps -q) && docker rm $(docker ps -aq)
-else
-    docker run -d -p 5000:5000 $IMAGE_NAME:$IMAGE_TAG
+#!/bin/bash
+IMAGE_NAME=cyberslot/app-repo
+IMAGE_TAG=python-app-1.0
+
+if [ "$(docker ps -q)" != "" ] 
+then 
+  echo "There are running containers" 
+  echo "Stopping and removing..."
+  docker stop $(docker ps -q) && docker rm $(docker ps -aq)
+  echo "Deploying..."
+  docker run -d -p 5000:5000 $IMAGE_NAME:$IMAGE_TAG
+elif [ "$(docker ps -aq)" != "" ]
+then 
+  echo "There are stopped containers" 
+  echo "Removing..."
+  docker rm $(docker ps -aq)
+  echo "Deploying..."
+  docker run -d -p 5000:5000 $IMAGE_NAME:$IMAGE_TAG
+else 
+  echo "No containers available"
+  echo "Deploying..."
+  docker run -d -p 5000:5000 $IMAGE_NAME:$IMAGE_TAG
 fi
