8e0c6360bbe0be63527183119a18f82f13ea9c4f
cyberslot <cyberslot@gmail.com>
2024-01-15 20:46:43 +0200

    Add PS script file

---
+++ psRulesScript.ps1
@@ -0,0 +1,25 @@
+# Import the PSRules module
+Import-Module -Name PSRules
+
+# Define the rules to be executed
+$rules = @(
+  @{
+      Name = 'Rule1'
+      ScriptBlock = {
+        # Rule 1 logic here
+				# e.g. check if a file exists
+				if (-not (Test-Path -Path 'C:\path\to\file.txt')) {
+					Write-Error 'File does not exist'
+      	}
+  		},
+  @{
+    	Name = 'Rule2'
+    	ScriptBlock = {
+        # Rule 2 logic here
+			}
+    }	
+  }
+)
+
+# Run the PSRules
+Invoke-PSRule -Rule $rules -Path 'C:\Path\To\Files'
